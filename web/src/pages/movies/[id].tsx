711cb1c066a2b37867546e067faceedceb491498
ivopereira-jr <ivopereira.jr12@gmail.com>
2023-09-25 13:44:56 -0300

    fix: Adjustment structure the app

--- web/src/pages/movies/[id].tsx
+++ web/src/pages/movies/[id].tsx
@@ -1,9 +1,5 @@
-import RootLayout from "@/app/layout"
-
 export default function MovieDetails(){
   return(
-    <RootLayout>
-      <p>teste</p>
-    </RootLayout>
+      <p>MovieDetails</p>
   )
 }
